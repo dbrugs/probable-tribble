c6de449534d7d9e0917a34765c3b6950b2a7d710
dbrugs <48110999+dbrugs@users.noreply.github.com>
2021-01-13 21:29:51 -0600

    Update webdriver.py

--- webdriver.py
+++ webdriver.py
@@ -2,6 +2,6 @@ from selenium import webdriver
 import atexit
 
 def make_webdriver():
-    browser = webdriver.Firefox(executable_path = '/home/brugs_bunny/webdriver/bin/geckodriver')
+    browser = webdriver.Firefox(executable_path = 'pathtodriver')
     atexit.register(lambda: browser.quit())
     return browser
